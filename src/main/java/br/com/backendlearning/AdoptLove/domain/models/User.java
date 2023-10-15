5aacac2dd40d5c5da32dd84f85507ce84421bccf
Samuel S. Furtado <salessamuel268@gmail.com>
2023-10-15 18:50:25 -0300

    feat: add User model in domain

---
+++ src/main/java/br/com/backendlearning/AdoptLove/domain/models/User.java
@@ -0,0 +1,25 @@
+package br.com.backendlearning.AdoptLove.domain.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "adopt-love")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    // LocalDate dateBirth
+    private String email;
+    private String password;
+
+}
