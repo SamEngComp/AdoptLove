f7de7404b43e02620624eb4ed5d4961417e3ae5b
Samuel S. Furtado <salessamuel268@gmail.com>
2023-10-15 21:41:40 -0300

    refactor: adjust in the SignUpController

--- src/main/java/br/com/backendlearning/AdoptLove/presentation/controller/SignUpController.java
+++ src/main/java/br/com/backendlearning/AdoptLove/presentation/controller/SignUpController.java
@@ -1,6 +1,12 @@
 package br.com.backendlearning.AdoptLove.presentation.controller;
 
+import br.com.backendlearning.AdoptLove.data.usecase.DbAddUser;
+import br.com.backendlearning.AdoptLove.domain.models.AddUser;
+import br.com.backendlearning.AdoptLove.domain.usecase.AddUserUseCase;
+import br.com.backendlearning.AdoptLove.infra.repository.AddUserPostgresRepository;
 import br.com.backendlearning.AdoptLove.presentation.models.SignUpFormModel;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sign-up")
 public class SignUpController {
 
+    @Autowired
+    private AddUserUseCase useCase;
+
     @PostMapping
+    @Transactional
     public void createUser(@RequestBody SignUpFormModel model) {
-        System.out.println(model);
+        useCase.createUser(makeAddUser(model));
+    }
+
+    private AddUser makeAddUser(SignUpFormModel model) {
+        return new AddUser(model.name(),
+                model.email(),
+                model.password(),
+                model.passwordConfirmation());
     }
 
 }
